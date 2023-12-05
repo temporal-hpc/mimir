1748fbd5ccbee35fa824c04b7cc1a5ece67ac741
Francisco Carter <francisco.carter@ug.uchile.cl>
2023-12-05 17:30:32 -0300

    Restore TensorCA CMakeLists
    Add voronoi code

--- samples/TensorCA-with_radius/asd.sh
+++
@@ -1,8 +0,0 @@
-./debug/prog 0 $((2**14)) 0 1 1 1
-./debug/prog 0 $((2**14)) 1 1 1 1
-./debug/prog 0 $((2**14)) 2 1 1 1
-./debug/prog 0 $((2**14)) 3 1 1 1
-./debug/prog 0 $((2**14)) 4 1 1 1
-./debug/prog 0 $((2**14)) 5 1 1 1
-./debug/prog 0 $((2**14)) 7 1 1 1
-./debug/prog 0 $((2**14)) 8 1 1 1
