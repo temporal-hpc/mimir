342df565ed38ed94065f44d3507939650538f166
Francisco Carter <frcarter.araya@gmail.com>
2024-03-12 18:35:42 -0300

    Compile glfw3 from source

--- samples/texture_cudaarray/helper_math.h
+++ samples/texture_cudaarray/helper_math.h
@@ -1139,28 +1139,6 @@ inline __host__ __device__ uint4 max(uint4 a, uint4 b)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// lerp
-// - linear interpolation between a and b, based on value t in [0, 1] range
-////////////////////////////////////////////////////////////////////////////////
-
-inline __device__ __host__ float lerp(float a, float b, float t)
-{
-    return a + t*(b-a);
-}
-inline __device__ __host__ float2 lerp(float2 a, float2 b, float t)
-{
-    return a + t*(b-a);
-}
-inline __device__ __host__ float3 lerp(float3 a, float3 b, float t)
-{
-    return a + t*(b-a);
-}
-inline __device__ __host__ float4 lerp(float4 a, float4 b, float t)
-{
-    return a + t*(b-a);
-}
-
-////////////////////////////////////////////////////////////////////////////////
 // clamp
 // - clamp the value v to be in the range [a, b]
 ////////////////////////////////////////////////////////////////////////////////
