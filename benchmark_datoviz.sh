463f6b7023f3e483fa53ff2e7852fb9854fdb1b8
Francisco Carter <francisco.carter@ug.uchile.cl>
2026-07-20 22:41:41 -0400

    Raise body count in nbody benchmarks

--- benchmark_datoviz.sh
+++ benchmark_datoviz.sh
@@ -12,21 +12,21 @@ echo "mode,windowres,grid_w,grid_h,iters,seed,density,framerate_fps,compute_time
 for exp in {6..15}; do
     size=$((2**${exp}))
     echo "    size: ${size}"
-    ./samples/ca/build/bin/benchmark_datoviz ${height} ${height} ${size} ${size} ${seed} 0.07 ${niter} >> ${file}
+    ./samples/ca/build/bin/benchmark_datoviz ${height} ${height} ${size} ${size} ${seed} 0.07 ${niter} --timeout 60 >> ${file}
 done
 
-sizes=(10000 25000 50000 75000 100000 125000 150000 175000 200000 225000 250000)
+sizes=(100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000)
 echo "Running datoviz nbody benchmark"
 file=${folder}/nbody_datoviz.csv
 echo "mode,windowres,N,iters,framerate_fps,compute_time_s,pipeline_time_s,graphics_time_s,vk_usage_gb,vk_budget_gb,gpu_power_w,gpu_energy_j,gpu_time_s,nvml_free_gb,nvml_reserved_gb,nvml_total_gb,nvml_used_gb,pack_time_s,d2h_time_s,h2h_time_s" >> ${file}
 for size in ${sizes[@]}; do
     echo "    size: ${size}"
-    ./samples/nbody-datoviz/build/bin/benchmark_datoviz ${width} ${height} ${size} ${niter} >> ${file}
+    ./samples/nbody-datoviz/build/bin/benchmark_datoviz ${width} ${height} ${size} ${niter} --timeout 60 >> ${file}
 done
 
 modes=(flat phong)
 options=("" "--light-model phong")
-kmodal_common="--pcolor 1.0,0.05,0.05 --background 1.0 --k 64 --epsilon 0.07 --fly --size 3"
+kmodal_common="--pcolor 1.0,0.05,0.05 --background 1.0 --k 64 --epsilon 0.07 --fly --size 1"
 echo "Running datoviz particles benchmark"
 for i in {0..1}; do
     mode=${modes[${i}]}
@@ -36,6 +36,6 @@ for i in {0..1}; do
     for exp in {4..8}; do
         size=$((10**${exp}))
         echo "    mode: ${mode}; size: ${size}"
-        ./samples/particles-kmodal-3d/build/bin/benchmark_datoviz ${width} ${height} ${size} ${seed} ${niter} ${kmodal_common} ${opts} >> ${file}
+        ./samples/particles-kmodal-3d/build/bin/benchmark_datoviz ${width} ${height} ${size} ${seed} ${niter} ${kmodal_common} ${opts} --timeout 60 >> ${file}
     done
 done
--- benchmark_mimir.sh
+++ benchmark_mimir.sh
@@ -17,33 +17,33 @@ fi
 echo "Running mimir CA benchmark"
 file=${folder}/ca_mimir_${mode}.csv
 echo "mode,windowres,grid_w,grid_h,iters,seed,density,framerate_fps,compute_time_s,pipeline_time_s,graphics_time_s,vk_usage_gb,vk_budget_gb,gpu_power_w,gpu_energy_j,gpu_time_s,nvml_free_gb,nvml_reserved_gb,nvml_total_gb,nvml_used_gb,pack_time_s,d2h_time_s,h2h_time_s" >> ${file}
-for exp in {6..15}; do
+for exp in {6..16}; do
     size=$((2**${exp}))
     echo "    mode: ${mode}; size: ${size}"
-    ./samples/ca/build/bin/benchmark_mimir ${height} ${height} ${size} ${size} ${seed} ${density} ${niter} --interop-sync ${sync} >> ${file}
+    ./samples/ca/build/bin/benchmark_mimir ${height} ${height} ${size} ${size} ${seed} ${density} ${niter} --timeout 60 --interop-sync ${sync} >> ${file}
 done
 
 echo "Running mimir nbody benchmark"
-sizes=(10000 25000 50000 75000 100000 125000 150000 175000 200000 225000 250000)
+sizes=(100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000)
 file=${folder}/nbody_mimir_${mode}.csv
 echo "mode,windowres,N,iters,framerate_fps,compute_time_s,pipeline_time_s,graphics_time_s,vk_usage_gb,vk_budget_gb,gpu_power_w,gpu_energy_j,gpu_time_s,nvml_free_gb,nvml_reserved_gb,nvml_total_gb,nvml_used_gb,pack_time_s,d2h_time_s,h2h_time_s" >> ${file}
 for size in ${sizes[@]}; do
     echo "    mode: ${mode}; size: ${size}"
-    ./samples/nbody/build/bin/benchmark ${width} ${height} ${size} ${niter} ${opts} --interop-sync ${sync} >> ${file}
+    ./samples/nbody/build/bin/benchmark ${width} ${height} ${size} ${niter} --timeout 60 --interop-sync ${sync} >> ${file}
 done
 
 echo "Running mimir particles benchmark"
-kmodal_opts="--pcolor 1.0,0.05,0.05 --background 1.0 --k 64 --epsilon 0.07 --fly --spp 1 --size 3"
+kmodal_opts="--pcolor 1.0,0.05,0.05 --background 1.0 --k 64 --epsilon 0.07 --fly --spp 1 --size 1"
 renders=(flat phong ptrace)
-options=("" "--light-model phong" "--light-model path-tracing")
+options=("--light-model none" "--light-model phong" "--light-model path-tracing")
 for i in {0..2}; do
     rend=${renders[${i}]}
     opts=${options[${i}]}
     file=${folder}/kmodal_mimir_${mode}_${render}.csv
     echo "mode,windowres,N,iters,seed,k,epsilon,framerate_fps,compute_time_s,pipeline_time_s,graphics_time_s,vk_usage_gb,vk_budget_gb,gpu_power_w,gpu_energy_j,gpu_time_s,nvml_free_gb,nvml_reserved_gb,nvml_total_gb,nvml_used_gb,pack_time_s,d2h_time_s,h2h_time_s,spp,bounces,subdiv,tlas_time_s,trace_time_s" >> ${file}
-    for exp in {4..8}; do
+    for exp in {4..9}; do
         size=$((10**${exp}))
         echo "    mode: ${mode}; size: ${size}"
-        ./samples/particles-kmodal-3d/build/bin/benchmark_mimir ${width} ${height} ${size} ${seed} ${niter} ${kmodal_opts} ${opts} >> ${file}
+        ./samples/particles-kmodal-3d/build/bin/benchmark_mimir ${width} ${height} ${size} ${seed} ${niter} ${kmodal_opts} ${opts} --timeout 60 --interop-sync ${sync} >> ${file}
     done
 done
